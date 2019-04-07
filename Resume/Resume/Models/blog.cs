78af84411c301745535c904d57450900ba0007ce
bijoybhaskarthakur <bijoybhaskarthakur721@gmail.com>
2019-04-07 12:33:02 +0530

    Resume Commit

---
+++ Resume/Resume/Models/blog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Resume.Models
+{
+    public class blog
+    {
+        public int ResumeId { get; set; }
+        public string name { get; set; }
+
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+
+        public State StudentState { get; set; }
+
+        public virtual List<post> posts { get; set; }
+
+    }
+
+    public enum State
+    {
+        a,
+        b,
+        c,
+        d,
+        e,
+        f
+    }
+
+}
