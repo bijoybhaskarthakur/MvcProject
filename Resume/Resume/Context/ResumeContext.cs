bf84c7affcf76150a06fea84e7fda3fd92895d70
bijoybhaskarthakur <bijoybhaskarthakur721@gmail.com>
2019-04-07 22:59:02 +0530

    commit code first approch

---
+++ Resume/Resume/Context/ResumeContext.cs
@@ -0,0 +1,15 @@
+﻿using Resume.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Resume.Context
+{
+    public class ResumeContext :DbContext
+    {
+        public DbSet<blog> Blogs { get; set; }
+        public DbSet<post> Posts { get; set; }  
+    }
+}
